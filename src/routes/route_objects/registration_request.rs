4ac3b561725fa56f6ca1954dfdf8d9439bbda602
Ilya Nixan <thenixan@gmail.com>
2021-06-30 19:00:26 +0300

    Explicitly set json field names

--- src/routes/route_objects/registration_request.rs
+++ src/routes/route_objects/registration_request.rs
@@ -3,6 +3,8 @@ use rocket::serde::Deserialize;
 #[derive(Deserialize, Debug, Clone)]
 #[serde(crate = "rocket::serde")]
 pub struct RegistrationRequest<'a> {
+    #[serde(rename = "login")]
     pub login: &'a str,
+    #[serde(rename = "password")]
     pub password: &'a str,
 }
